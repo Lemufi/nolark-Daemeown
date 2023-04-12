c58b868f006dbce105bc89bae0af6e9bc7e60e1a
Lemufi <59792742+Phenix220@users.noreply.github.com>
2023-04-12 12:32:33 +0200

    Bug index click himself

--- includes/lienspages.inc.php
+++ includes/lienspages.inc.php
@@ -1,21 +1,21 @@
 <?php
 
 function linkIndex() {
-    $pages = array("index.php", "route.php", "cross.php", "piste.php", "enfants.php", "nous-contacter.php");
+    $pages = array("index.php", "pages/route.php", "pages/cross.php", "pages/piste.php", "pages/enfants.php", "pages/nous-contacter.php");
     $noms = array("Accueil", "Route", "Cross", "Piste", "Enfants", "Nous contacter");
     $nbLiens = count($pages);
     for ($i = 0; $i < $nbLiens; $i++) {
         if ($pages === "index.php") {
             echo '<li><a href=',$pages[$i],'>', $noms[$i], '</a></li>';
         } else {
-            echo '<li><a href="pages/', $pages[$i], '">', $noms[$i], '</a></li>';
+            echo '<li><a href="', $pages[$i], '">', $noms[$i], '</a></li>';
         }
     }
 }
 
 function linkOther() {
     $pages = array("../index.php", "route.php", "cross.php", "piste.php", "enfants.php", "nous-contacter.php");
-    $noms = array("Acceuil", "Route", "Cross", "Piste", "Enfants", "Nous contacter");
+    $noms = array("Accueil", "Route", "Cross", "Piste", "Enfants", "Nous contacter");
     $nbLiens = count($pages);
     for ($i = 0; $i < $nbLiens; $i++) {
         echo '<li><a href="', $pages[$i], '">', $noms[$i], '</a></li>';
